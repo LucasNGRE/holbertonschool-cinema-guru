78afb734e66ce1ba996a8eefae2bb3cc2a518803
LucasNGRE <lucas12negre@gmail.com>
2026-02-23 09:41:10 +0100

    3. Initialize the main App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import GeneralComponentsDemo from "./routes/GeneralComponentsDemo";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
+import Dashboard from "./routes/Dashboard";
+import Authentication from "./routes/Authentication";
+
+export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) return;
+
+    axios
+      .post(
+        "http://localhost:8000/api/auth/",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      )
+      .then((res) => {
+        setIsLoggedIn(true);
+        setUserUsername(res.data.username ?? "");
+      })
+      .catch(() => {
+        setIsLoggedIn(false);
+        setUserUsername("");
+      });
+  }, []);
+
   return (
     <div className="App">
-      <GeneralComponentsDemo />
+      {isLoggedIn ? (
+        <Dashboard userUsername={userUsername} />
+      ) : (
+        <Authentication />
+      )}
     </div>
   );
 }
-
-export default App;
